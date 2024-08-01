0bcd605eeb1d97ece43c268813b6e6f9f4f8540b
Lukáš Procházka <32812890+Deathrage@users.noreply.github.com>
2024-08-01 23:56:56 +0200

    added support for blob storage for certificataes (#2)

--- DSDD.Automations.Payments.RBCZ/ServiceCollectionExtensions.cs
+++ DSDD.Automations.Payments.RBCZ/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using DSDD.Automations.Payments.RBCZ.PremiumApi;
+﻿using DSDD.Automations.Payments.RBCZ.Certificates;
+using DSDD.Automations.Payments.RBCZ.PremiumApi;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DSDD.Automations.Payments.RBCZ;
@@ -8,6 +9,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddPaymentsRBCZ(this IServiceCollection services)
         => services
             .AddApiClient()
+            .AddAccountCertificateProvider()
             .AddSingleton<IFxRates, FxRates>()
             .AddTransient<IBankPaymentsImporter, BankPaymentsImporter>();
 }
